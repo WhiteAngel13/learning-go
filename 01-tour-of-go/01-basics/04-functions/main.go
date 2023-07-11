06ec0dcefd0a90466088694544aa709245dc6436
whiteangel13 <edmilsonsw22@gmail.com>
2023-07-11 19:22:17 -0400

    feat: implements basics functions

--- 01-tour-of-go/01-basics/04-functions/main.go
+++ 01-tour-of-go/01-basics/04-functions/main.go
@@ -2,10 +2,28 @@ package main
 
 import "fmt"
 
-func add(x int, y int) int {
+func add_with_each_values_typed(x int, y int) int {
 	return x + y
 }
 
+func add_with_all_values_typed(x, y int) int {
+	return x + y
+}
+
+func multi_value_returned_swap(x, y string) (string, string) {
+	return y, x
+}
+
+func named_return_values_split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return
+}
+
 func main() {
-	fmt.Println(add(42, 13))
+	fmt.Println(add_with_each_values_typed(42, 13))
+	fmt.Println(add_with_all_values_typed(42, 13))
+	a, b := multi_value_returned_swap("hello", "world")
+	fmt.Println(a, b)
+	fmt.Println(named_return_values_split(84))
 }
